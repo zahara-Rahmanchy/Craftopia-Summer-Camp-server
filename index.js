696ad3093482e07339887c9840dc239c787b062e
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-06-08 09:45:39 +0600

    jwt token added

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
+// jwt
+const verifyJWT = (req, res, next) => {
+  // we will first check there is authorization, if not then it means not a valid user
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error: true, message: "Unauthorized Access!"});
+  }
+  // the token is in this form bearer space token, so split using space
+
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({error: true, message: "Unauthorized Access!"});
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofsmeh8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,9 +54,18 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log("jwt user", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "2h",
+      });
+      res.send({token});
+    });
 
     // users collection:all student,instructor,admin:viewed to admin only
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyJWT, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
